224b3f07c278127c58e44a0475d05d9a4776c90f
Monica Davila <monica.davila510@gmail.com>
2026-06-01 15:21:08 -0500

    Add get single project route

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -30,4 +30,22 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
+// GET single project by ID
+router.get("/:id", auth, async (req, res) => {
+  try {
+    const project = await Project.findOne({
+      _id: req.params.id,
+      owner: req.user.id,
+    });
+
+    if (!project) {
+      return res.status(404).json({ message: "Project not found" });
+    }
+
+    res.json(project);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
 module.exports = router;
