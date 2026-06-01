d8e96bc9a23215fdd5ba170dbbc925cc2d618d67
Monica Davila <monica.davila510@gmail.com>
2026-06-01 10:34:39 -0500

    Complete CRUD operations for tasks

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -25,4 +25,28 @@ router.post("/", async (req, res) => {
   }
 });
 
+// UPDATE a task
+router.put("/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    res.json(task);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// DELETE a task
+router.delete("/:id", async (req, res) => {
+  try {
+    await Task.findByIdAndDelete(req.params.id);
+
+    res.json({ message: "Task deleted" });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
 module.exports = router;
