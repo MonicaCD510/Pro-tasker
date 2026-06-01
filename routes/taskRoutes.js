03dc1977a43da8521798f75c034a147fec60bb0d
Monica Davila <monica.davila510@gmail.com>
2026-06-01 10:22:15 -0500

    Add GET and POST task endpoints

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,9 +1,28 @@
 const express = require("express");
+const Task = require("../models/Task");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ message: "Task routes working" });
+// GET all tasks
+router.get("/", async (req, res) => {
+  try {
+    const tasks = await Task.find();
+
+    res.json(tasks);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// CREATE a task
+router.post("/", async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+
+    res.status(201).json(task);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 });
 
 module.exports = router;
